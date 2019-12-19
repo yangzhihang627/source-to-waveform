ddc2af8bfbf5aa30ad29da81fdd763ebb17e65e0
yangzhihang <19014878@suning.com>
2019-12-19 16:46:38 +0800

    完成生成svg模块

--- src/lib/source-svg-waveform.ts
+++ src/lib/source-svg-waveform.ts
@@ -9,166 +9,176 @@ const path = require('path');
 const process = require('process');
 const fs = require('fs');
 const OUTPUT_DIR = path.resolve(process.cwd(), 'dist');
+const EventEmitter = require('events').EventEmitter;
 
-interface Data {
-  filePath: string,
-  minute?: number,
-  cancelCB?: Function
+interface Vidio2SvgProps {
+  source: string;
+  minute?: number;
 }
 
-export default class SourceSVGWaveform  {
-    filePath: string;
-    outputDirName: string;
-    minute: number;
-    count: number;
-    outputFullPath: string;
-    section: number;
-    cancelFlag: boolean;
-    ffCommand: object | null;
-    cancelCB: Function;
-    audioUrl: string;
-    cancelDisabled: boolean;
-    fullPaths: string[];
-    fullDuration: number;
-    firstDuration: number;
-    constructor (props: Data) {
-        this.filePath = props.filePath;
-        this.minute = props.minute || 3;
-        this.count = 0;
-        this.outputDirName = '';
-        this.outputFullPath = '';
-        this.section = 1;
-        this.cancelFlag = false;
-        this.cancelCB = props.cancelCB || (() => {});
-        this.ffCommand = null;
-        this.audioUrl = '';
-        this.cancelDisabled = true;
-        this.fullPaths = [];
-        this.fullDuration = 0;
-        this.firstDuration = 0;
+export default class SourceSVGWaveform extends EventEmitter {
+  filePath: string;
+  outputDirName: string;
+  minute: number;
+  count: number;
+  outputFullPath: string;
+  section: number;
+  cancelFlag: boolean;
+  ffCommand: object | null;
+  cancelCallback: Function | null;
+  audioUrl: string;
+  cancelDisabled: boolean;
+  fullPaths: string[];
+
+  constructor () {
+    super()
+    this.filePath = '';
+    this.minute = 3;
+    this.count = 0;
+    this.outputDirName = '';
+    this.outputFullPath = '';
+    this.section = 1;
+    this.cancelFlag = false;
+    this.cancelCallback = null;
+    this.ffCommand = null;
+    this.audioUrl = '';
+    this.cancelDisabled = true;
+    this.fullPaths = [];
+  }
+
+  private delDir (path: string) {
+    let files = [];
+    if(fs.existsSync(path)){
+      files = fs.readdirSync(path);
+      files.forEach((file: any) => {
+        let curPath = path + "/" + file;
+        if(fs.statSync(curPath).isDirectory()){
+          this.delDir(curPath); //递归删除文件夹
+        } else {
+          fs.unlinkSync(curPath); //删除文件
+        }
+      });
+      fs.rmdirSync(path);
     }
+  }
 
-    private delDir = path => {
-        let files = [];
-        if(fs.existsSync(path)){
-          files = fs.readdirSync(path);
-          files.forEach((file, index) => {
-            let curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()){
-              this.delDir(curPath); //递归删除文件夹
-            } else {
-              fs.unlinkSync(curPath); //删除文件
-            }
-          });
-          fs.rmdirSync(path);
-        }
-      }
-
-    video2Audio = () => {
-        // 素材位置
-        const fileName: string = this.filePath.slice(this.filePath.lastIndexOf('/') + 1, this.filePath.lastIndexOf('.'));
-        const startTime: number = Date.now(); //开始计时
-        this.outputDirName = `${OUTPUT_DIR}/${fileName}-${startTime}`;
-        fs.mkdirSync(this.outputDirName);
-        ffmpeg.ffprobe(this.filePath, (err, metadata) => {
-          if (!err) {
-            const audioMsg = metadata.streams.filter((item) => item.codec_type === 'audio')[0];
-            const audioType: string = audioMsg.codec_name;
-            const audioDuration: any = audioMsg.duration;
-            let extension: string;
-            switch (audioType){
-              case 'vorbis':
-                extension = 'ogg';
-                break;
-              default:
-                extension = audioType;
-            }        
-            this.outputFullPath = `${OUTPUT_DIR}/${fileName}-${startTime}.${extension}`;
-            const command = ffmpeg();
-            this.section = Math.ceil(audioDuration / (60 * this.minute)), //分割的总份数
-            this.ffCommand = command;
-            this.cancelDisabled = false;
-            command.input(this.filePath).noVideo().audioCodec('copy') // 生成整段音频
-            .on('error', (err) => {
-              console.log(`转化失败: "${err.message}"`);
-            })
-            .on('end', () => {
-              this.audioUrl = `${fileName}-${startTime}.${extension}`
-            }).output(this.outputFullPath).run();
-            this.splitAudio(fileName, extension, startTime); // 开始分割音频文件
-          } else {
-            console.log('未找到音频流！');
-          }
-        })
-      }
-    
-      private splitAudio = (fileName: string, extension: string, startTime: number) => {
-        const outputName = `${fileName}-${Date.now()}.${extension}`;
-        const outputUrl = `${fileName}-${startTime}/${outputName}`;
-        const outputPath = `${OUTPUT_DIR}/${outputUrl}`;
-        const unit = 60 * this.minute; // 分割单位
-        if (this.count >= this.section) { // 计数器小于长度时持续分割
-          console.log('音频分割完成');
-          return;
-        }
-        const command = ffmpeg()  // 开始分割每段音频
-        command.input(this.filePath).noVideo().audioCodec('copy')
+  vidio2Svg (props: Vidio2SvgProps) {
+    this.fullPaths = [] // svg数据清空
+    this.filePath = props.source;
+    props.minute && (this.minute = props.minute)
+    const fileName: string = this.filePath.slice(this.filePath.lastIndexOf('/') + 1, this.filePath.lastIndexOf('.'));
+    const startTime: number = Date.now(); //开始计时
+    this.outputDirName = `${OUTPUT_DIR}/${fileName}-${startTime}`;
+    fs.mkdirSync(this.outputDirName);
+    ffmpeg.ffprobe(this.filePath, (err, metadata) => {
+      this.emit('start', startTime)
+      if (!err) {
+        const audioMsg = metadata.streams.filter((item) => item.codec_type === 'audio')[0];
+        const audioType: string = audioMsg.codec_name;
+        const audioDuration: any = audioMsg.duration;
+        let extension: string;
+        switch (audioType){
+          case 'vorbis':
+            extension = 'ogg';
+            break;
+          default:
+            extension = audioType;
+        }        
+        this.outputFullPath = `${OUTPUT_DIR}/${fileName}-${startTime}.${extension}`;
+        const command = ffmpeg();
+        this.section = Math.ceil(audioDuration / (60 * this.minute)), //分割的总份数
+        this.emit('getSection', this.section)
+        this.ffCommand = command;
+        this.cancelDisabled = false;
+        command.input(this.filePath).noVideo().audioCodec('copy') // 生成整段音频
         .on('error', (err) => {
           console.log(`转化失败: "${err.message}"`);
         })
         .on('end', () => {
-          if (this.cancelFlag) { // 终止分割音频
-            setTimeout(() => { // 因为有连环回掉延后重置
-              this.cancelCB();
-            }, 1000);
-            return;
-          }
-          this.audio2Svg(outputUrl, startTime);
-          this.count = this.count + 1;
-          this.splitAudio(fileName, extension, startTime);
-        }).output(outputPath)
-        .seek(unit * this.count)
-        .duration(unit)
-        .run()
+          this.audioUrl = `${fileName}-${startTime}.${extension}`
+          this.emit('getAudioData', this.audioUrl, this.outputFullPath)
+        }).output(this.outputFullPath).run();
+        this.splitAudio(fileName, extension, startTime); // 开始分割音频文件
+      } else {
+        console.log('未找到音频流！');
       }
-    
-      audio2Svg = (outputUrl, startTime) => {
-        const trackWaveform = new AudioSVGWaveform({
-          sampleRate: 3000,
-          url: outputUrl,
-          buffer: null
-        });
-        trackWaveform.loadFromUrl().then(() => {
-          const data: any = trackWaveform.getPath();
-          const d: string = data.d;
-          const endTime: number = data.timestamp;
-    
-          if (this.cancelFlag) { // 终止生成音频svg
-            setTimeout(() => { // 因为有连环回掉延后重置
-              this.cancelCB();
-            }, 1000);
-            return;
-          }
-          switch (this.count) {
-            case this.section - 1: //全部svg生成时间
-                this.fullDuration = endTime - startTime;
-                this.cancelDisabled = true;
-                this.delDir(this.outputDirName);
-                break;
-            case 0: // 第一段svg生成时间
-                this.firstDuration = endTime - startTime
-                break;
-          }
-          this.fullPaths.push(d)
-          console.log(this.count, this.fullPaths)
-        });
+    })
+    return this;
+  }
+  
+  private splitAudio (fileName: string, extension: string, startTime: number) {
+    const outputName = `${fileName}-${Date.now()}.${extension}`;
+    const outputUrl = `${fileName}-${startTime}/${outputName}`;
+    const outputPath = `${OUTPUT_DIR}/${outputUrl}`;
+    const unit = 60 * this.minute; // 分割单位
+    if (this.count >= this.section) { // 计数器小于长度时持续分割
+      return;
+    }
+    const command = ffmpeg()  // 开始分割每段音频
+    command.input(this.filePath).noVideo().audioCodec('copy')
+    .on('error', (err) => {
+      console.log(`转化失败: "${err.message}"`);
+    })
+    .on('end', () => {
+      if (this.cancelFlag) { // 终止分割音频
+        setTimeout(() => { // 因为有连环回掉延后重置
+          this.reset();
+          this.cancelCallback && this.cancelCallback();
+        }, 1000);
+        return;
       }
+      this.audio2Svg(outputUrl, startTime);
+      this.count = this.count + 1;
+      this.splitAudio(fileName, extension, startTime);
+    }).output(outputPath)
+    .seek(unit * this.count)
+    .duration(unit)
+    .run()
+  }
+  
+  private audio2Svg (outputUrl: string, startTime: number) {
+    const trackWaveform = new AudioSVGWaveform({
+      sampleRate: 3000,
+      url: outputUrl,
+      buffer: null
+    });
+    trackWaveform.loadFromUrl().then(() => {
+      const data: any = trackWaveform.getPath();
+      const d: string = data.d;
+      const endTime: number = data.timestamp;
+      if (this.cancelFlag) { // 终止生成音频svg
+        setTimeout(() => { // 因为有连环回掉延后重置
+          this.reset();
+          this.cancelCallback && this.cancelCallback();
+        }, 1000);
+        return;
+      }
+      if (this.fullPaths.length === this.section - 1) {
+        this.reset()
+        this.emit('end', endTime)
+      }
+      this.fullPaths.push(d)
+      this.emit('getSvg', this.fullPaths)
+    });
+      
+  }
 
-      cancel = () => {
-        this.cancelFlag = true; // 终止分割音频
-        const command: any = this.ffCommand;
-        command.kill(); // 终止生成整段音频
-        this.delDir(this.outputDirName) // 删除分割音频的文件夹
-        fs.unlinkSync(this.outputFullPath); // 删除整段音频
-      }
+  private reset = () => {
+    this.count = 0;
+    this.filePath = '';
+    this.audioUrl = '';
+    this.cancelDisabled = true;
+    this.ffCommand = null;
+    this.delDir(this.outputDirName);
+  }
+
+  cancel = (callback: Function) => {
+    callback && (this.cancelCallback = callback)
+    this.cancelFlag = true; // 终止分割音频
+    const command: any = this.ffCommand;
+    command.kill(); // 终止生成整段音频
+    this.delDir(this.outputDirName) // 删除分割音频的文件夹
+    fs.unlinkSync(this.outputFullPath); // 删除整段音频
+    return this;
+  }
 }
